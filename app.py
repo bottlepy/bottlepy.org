5f35a3b02ddeabb022aa08ea121f8c67c45658bb
Marcel Hellkamp <marc@gsites.de>
2012-02-29 23:03:53 +0100

    Removed some old redirects.

--- app.py
+++ app.py
@@ -1,61 +1,38 @@
 #!/usr/bin/env python2.6
 # -*- coding: utf-8 -*-
 
-import functools
+import os
 import bottle
 
 app = application = bottle.Bottle()
 
+docs_path   = os.path.join(os.path.dirname(__file__), 'docs/')
+static_path = os.path.join(os.path.dirname(__file__), 'static/')
 
-# Permanent redirects (303)
+@app.get('/')
+def index():
+    bottle.redirect('/docs/dev/')
 
-redirects = '''
-/page/start     /docs/dev/index.html
-/page/docs      /docs/dev/tutorial.html
-/page/tutorial  /docs/dev/tutorial_app.html
-/api            /docs/dev/
-/api/:fname#.*# /docs/dev/%(fname)s
-/               /docs/dev/
-/docs           /docs/dev/
-/docs/          /docs/dev/
-/docs/:ver      /docs/%(ver)s/
-'''.strip()
-
-def rdcb(target, **vars):
-    bottle.redirect(target % vars)
-
-for line in redirects.splitlines():
-    url, target = line.split()
-    tmp = functools.partial(rdcb, target)
-    functools.update_wrapper(tmp, rdcb)
-    app.route(url, callback=tmp)
-
-# Static API docs
-
-@app.get('/docs/:version/')
-@app.get('/docs/:version/:filename#.+#')
-def static(version, filename='index.html'):
-    return bottle.static_file(filename, root='./docs/%s/' % version)
-
-# Git commit redirect
+@app.get('/docs/<version>/')
+@app.get('/docs/<version>/<filename:path>')
+def docs(version, filename='index.html'):
+    filename = version + '/' + (filename or 'index.html')
+    print filename
+    return bottle.static_file(filename, root=docs_path)
 
 @app.get('/commit/:hash#[a-zA-Z0-9]+#')
 def commit(hash):
     url = 'https://github.com/defnull/bottle/commit/%s'
     bottle.redirect(url % hash.lower())
 
-# bottle.py redirect
-
 @app.get('/bottle.py')
 def download():
     url = 'https://github.com/defnull/bottle/raw/master/bottle.py'
     bottle.redirect(url)
 
-# Other static files
-
-@app.get('/:filename#.+\.(css|js|ico|png|txt|html)#')
+@app.get('/<filename:path>')
 def static(filename):
-    return bottle.static_file(filename, root='./static/')
+    return bottle.static_file(filename, root=static_path)
 
 # Start server
 if __name__ == '__main__':
