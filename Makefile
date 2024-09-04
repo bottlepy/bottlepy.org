f8a9721731ccdda1cda475655e100a90f9a5ab21
Marcel Hellkamp <marc@gsites.de>
2024-09-04 15:42:27 +0200

    Cleanup for 0.13 release

--- Makefile
+++ Makefile
@@ -1,20 +1,13 @@
 .PHONY: docs server bootstrap deploy
 
 docs:
-	./build_docs.sh master docs/dev
+	./build_docs.sh patch-py313-multipart docs/dev
 	./build_docs.sh release-0.12 docs/0.12
-	./build_docs.sh release-0.11 docs/0.11
+	#./build_docs.sh release-0.11 docs/0.11
 	#./build_docs.sh release-0.10 docs/0.10
 	#./build_docs.sh release-0.9 docs/0.9
 	cd docs; ln -fs 0.12 stable
 
-bootstrap:
-	wget -O /tmp/bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
-	unzip -d static/ /tmp/bootstrap.zip
-
-server:
-	./server restart
-
-deploy:
-	ssh marc@bottlepy.org 'cd bottlepy.org && git pull && make server && make docs'
+deploy: docs
+	rsync -av docs/ root@defnull.de:/srv/stack/nginx/vhost/bottlepy.org/docs/
 
--- build_docs.sh
+++ build_docs.sh
@@ -1,66 +1,33 @@
 #!/bin/bash
 
-
-##############################################################################
-# This script builds the bottle documentation for a specific git branch.
-##############################################################################
-
 cd $(dirname $0)
 set -e
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 branch output_folder"
-  echo "Example: $0 master docs/dev"
-  exit 0
-fi
-
-for cmd in git tar zip make rsync sphinx-build pdflatex makeindex; do
-  hash $cmd 2>&- || {
-    echo >&2 "Please install '$cmd' first. Aborting."
-    exit 1
-  }
-done
-
-repo_dir='/tmp/update_docs_bottle.git'
-build_dir='/tmp/update_docs_bottle.build'
 branch=$1
 output=$2
+venv_dir="./build/venv"
+repo_dir="./build/bottle.git"
+build_dir="./build/docs/"
 
-test -d $repo_dir || git clone git://github.com/bottlepy/bottle.git $repo_dir
-pushd $repo_dir
- git fetch origin
- git checkout -f "docs-$branch" || git checkout -f -b "docs-$branch" "origin/$branch"
- git clean -d -x -f
- git pull
- test -d apidocs && cd apidocs || cd docs
- docs=`pwd`
+mkdir -p "$build_dir"
+test -d "$venv_dir" || python3 -mvenv "$venv_dir"
+source "$venv_dir/bin/activate"
+pip install -r requirements.txt
+test -d $repo_dir || git clone https://github.com/bottlepy/bottle.git $repo_dir
+
+pushd "$repo_dir"
+  git fetch origin
+  git switch --detach --force "origin/$branch"
+  git clean -d -x -f
+  test -d apidocs && cd apidocs || cd docs
+  docs_dir=`pwd`
 popd
 
-PYTHONPATH=$repo_dir sphinx-build -q -c sphinx -b html     $docs $build_dir/$branch/html
-PYTHONPATH=$repo_dir sphinx-build -q -c sphinx -b latex    $docs $build_dir/$branch/latex
-PYTHONPATH=$repo_dir sphinx-build -q -c sphinx -b bottlepy $docs $build_dir/$branch/pickle
-
-pushd $build_dir/$branch
- pushd latex
-  pdflatex -halt-on-error Bottle.tex > latex.log
-  pdflatex -halt-on-error Bottle.tex >> latex.log
-  pdflatex -halt-on-error Bottle.tex >> latex.log
-  makeindex -s python.ist Bottle.idx >> latex.log
-  pdflatex -halt-on-error Bottle.tex >> latex.log
-  pdflatex -halt-on-error Bottle.tex >> latex.log
-  test -e Bottle.pdf || exit 1
- popd
- cp latex/Bottle.pdf bottle-docs.pdf
- tar -czf bottle-docs.tar.gz html
- tar -cjf bottle-docs.tar.bz2 html
- zip -q -r -9 bottle-docs.zip html
- cp bottle-docs.* html
- cp $repo_dir/bottle.py html
-popd
+rm -rf $build_dir/$branch
+PYTHONPATH=$repo_dir sphinx-build -E -q -c sphinx -b html $docs_dir $build_dir/$branch/html
+cp $repo_dir/bottle.py $build_dir/$branch/html
 
 # We rsync to keep mtime on unchanged files. Good for HTTP caching.
-test -d "$output/" || mkdir -p "$output/"
-rsync -vrc --exclude .doctrees $build_dir/$branch/html/ "$output/"
-rsync -vrc --exclude .doctrees $build_dir/$branch/pickle/ "$output/"
-rm -rf $build_dir/$branch
+mkdir -p "$output"
+rsync -vrc --exclude .doctrees --delete $build_dir/$branch/html/ "$output"
 
